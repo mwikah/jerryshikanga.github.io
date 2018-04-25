2374485fd4e14ef577d80afa1d606098c55e16f2
Jerry Shikanga <jerryshikanga@gmail.com>
2018-04-25 23:37:29 +0300

    Support for github calendar plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar(".github-graph", "jerryshikanga");
+    new GitHubCalendar(".github-graph", "jerryshikanga");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
